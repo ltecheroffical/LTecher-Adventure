5e6a8e06509ed8c8571183681a0515a176e96ace
Lawrence <82725076+ltecheroffical@users.noreply.github.com>
2024-01-05 03:48:14 -0500

    Fix git ignore

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+#include <raylib.h>
+
+#include <glm/glm.hpp>
+
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
+
+int main()
+{
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "LTecher Adventure");
+
+	SetWindowState(FLAG_WINDOW_RESIZABLE);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+
+		ClearBackground({0, 255, 255, 255});
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
+}
