0d0e711ff8b5a4fa3f514b4c4efb76c4e07bb5a5
Lawrence <82725076+ltecheroffical@users.noreply.github.com>
2024-01-12 02:38:27 -0500

    Fix squish on healthbar and compact health code

--- src/gui/health_bar.cpp
+++ src/gui/health_bar.cpp
@@ -19,9 +19,9 @@ void HealthBar::on_render()
 {
     auto health_bar_width = static_cast<float>(texture_health_atlas.width);
 
-    // Health Bar Fill
-    DrawTexturePro(HealthBar::texture_health_atlas, {0, 120, health_bar_width, 120}, {0, 0, health_bar_width * (this->health.get_health() / this->health.get_max()) * this->scale + this->size.x, 120  * this->scale + this->size.y}, { -this->position.x, -this->position.y }, 0, WHITE);
-    
     // Health Bar Frame
     DrawTexturePro(HealthBar::texture_health_atlas, {0, 0, health_bar_width, 120}, {0, 0, health_bar_width * this->scale + this->size.x, 120 * this->scale + this->size.y}, { -this->position.x, -this->position.y }, 0, WHITE);
+    
+    // Health Bar Fill
+    DrawTexturePro(HealthBar::texture_health_atlas, {0, 120, health_bar_width * this->health.get_health(true), 120}, {0, 0, health_bar_width * this->health.get_health(true) * this->scale + this->size.x, 120  * this->scale + this->size.y}, { -this->position.x, -this->position.y }, 0, WHITE);
 }
