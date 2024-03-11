054ce2dff60c2ad88a5a8ac5b3bd3ffebc458ce9
Lawrence <82725076+ltecheroffical@users.noreply.github.com>
2024-03-11 06:53:51 -0400

    Add vector comparsions

--- src/marcos/compare.h
+++ src/marcos/compare.h
@@ -1,3 +1,5 @@
+#include <raylib.h>
+
 #define CLRCMP(c1, c2) (((Color)c1).r == ((Color)c2).r &&\
   ((Color)c1).g == ((Color)c2).g &&\
   ((Color)c1).b == ((Color)c2).b)
@@ -5,3 +7,7 @@
   ((Color)c1).g == ((Color)c2).g &&\
   ((Color)c1).b == ((Color)c2).b &&\
   ((Color)c1).a == ((Color)c2).a)
+
+#define VEC2CMP(v1, v2) (((Vector2)v1).x == ((Vector2)v2).x &&\
+  ((Vector2)v1).y == ((Vector2)v2).y &&\
+  ((Vector2)v1).z == ((Vector2)v2).z)
